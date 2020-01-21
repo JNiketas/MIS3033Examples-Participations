099c483b61eed3d88588714cf3e913e89c2b490b
Nike0000 <Nike0000@PCB-LAB360-25>
2020-01-21 12:30:38 -0600

    Created Solution and project

---
+++ InClassExamples/DataAccess/Program.cs
@@ -0,0 +1,16 @@
+﻿//John Niketas
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
