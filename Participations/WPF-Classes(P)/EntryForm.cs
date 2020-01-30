343348cf28eadd356eed4dea17731edfa60a7a50
Nike0000 <Nike0000@PCB-LAB360-25>
2020-01-30 12:30:25 -0600

    Finished WPF Participation

---
+++ Participations/WPF-Classes(P)/EntryForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Classes_P_
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public EntryForm()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            string tostring = $"{Name}, {Address} - {ZipCode}";
+            return tostring;
+        }
+    }
+}
---
+++ Participations/WPF-Classes(P)/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes_P_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            string Name, Address, ZipCodeString;
+            int ZipCode;
+
+            Name = txtName.Text;
+            Address = txtAddress.Text;
+            ZipCodeString = txtZipcode.Text;
+
+            ZipCode = Convert.ToInt32(ZipCodeString);
+
+            EntryForm EF = new EntryForm();
+            EF.Address = Address;
+            EF.Name = Name;
+            EF.ZipCode = ZipCode;
+
+            lstForm.Items.Add(EF);
+        }
+    }
+}
