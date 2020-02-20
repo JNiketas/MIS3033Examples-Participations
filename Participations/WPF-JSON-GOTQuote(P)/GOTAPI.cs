7595cbff196481f5bcd56308e0953232fce89ba7
Nike0000 <Nike0000@PCB-LAB360-25>
2020-02-20 13:14:51 -0600

    GOT WPF JSON Participation

---
+++ Participations/WPF-JSON-GOTQuote(P)/GOTAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_JSON_GOTQuote_P_
+{
+    class GOTAPI
+    {
+       public string quote { get; set; }
+        public string character { get; set; }
+
+
+    }
+}
